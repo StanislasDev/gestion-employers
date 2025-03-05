9e0340b6a8a34cf80a7c89f90899db58b02f31f2
Stanislas <simostanley91@gmail.com>
2025-03-05 11:04:31 +0100

    Liste des employers >> affichage du formulaire de creation d'un employer

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Employer;
+use App\Models\Departement;
 use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $totalDepatements = Departement::all()->count();
+        $totalEmployers = Employer::all()->count();
+        $totalUsers = User::all()->count();
+
+        return view('dashboard', compact('totalDepatements', 'totalEmployers', 'totalUsers'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,22 @@
 
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthController::class, 'login'])->name('login');
-Route::post('/', [AuthController::class, 'handleLogin'])->name('handleLogin');
+Route::middleware('guest')->group(function(){
+    Route::get('/', [AuthController::class, 'login'])->name('login');
+    Route::post('/', [AuthController::class, 'handleLogin'])->name('handleLogin');
+});
 
-Route::get('/dashboard', [AppController::class, 'index'])->name('index');
+
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard', [AppController::class, 'index'])->name('index');
+
+    Route::prefix('employers')->group(function(){
+        Route::get('/', [EmployerController::class, 'index'])->name('employer.index');
+        Route::get('/create', [EmployerController::class, 'create'])->name('employer.create');
+        Route::get('/edit/{employer}', [EmployerController::class, 'edit'])->name('employer.edit');
+    });
+});
+
